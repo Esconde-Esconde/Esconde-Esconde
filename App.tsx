061af5fe2ac5254942ad8d6219ccf455957d96ad
NicolasPietroDEV <nicolaspietrol.souza@gmail.com>
2023-10-02 18:28:49 -0300

    teste

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Joguinho Plays!</Text>
+      <Text>Joguinho Play!</Text>
       <StatusBar style="auto" />
     </View>
   );
